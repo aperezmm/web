b3824d77e9fa1cefa273ae9d7d5430554e7c1ce8
Alejandro Perez <alejandro.perez1@udea.edu.co>
2021-06-30 18:46:30 -0500

    first screen

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withTM = require('next-transpile-modules')(['@devperez/components'])
+
+module.exports = withTM()
+
+//NEXT JS
+
+/*
+Tanto el codigo del frontend como el del backend se ejecutan simultaneamente. (Isomorfico)
+Como FULLSTACK tener en cuenta lo que ocurre en Javascript cliente y Javascript lado servidor.
+*Es una librería
+*Soporta estilos globales en pages/_app.js
+
+*Ideal para varios components, soluciona problema de routing,
+*/ 
